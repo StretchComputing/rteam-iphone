3375db5806a0995ec6ca14e11cb6e1e60f1bc80e
Nick Wroblewski <njw438@gmail.com>
2011-11-14 09:55:39 -0600

    final bug fix pre 3.0 version

---
+++ Classes/FastActionSheetPost.h
@@ -0,0 +1,13 @@
+//
+//  FastActionSheetPost.h
+//  rTeam
+//
+//  Created by Nick Wroblewski on 11/8/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FastActionSheetPost : UIViewController
+
+@end
